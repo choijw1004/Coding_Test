36ab53db906b590063c06407a4e015a295b2e31c
choijw1004 <97choijw@gmail.com>
2024-04-23 17:47:37 +0900

    20240423

--- src/boj_2638/Main.java
+++ src/boj_2638/Main.java
@@ -1,9 +1,32 @@
 package boj_2638;
 
+import java.util.Scanner;
+
 public class Main {
 
+	static int N;
+	static int M;
+	static int[][] map;
+	static int time;
+	static int c
+	// 상 하 좌 우
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		time = 0;
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
 
 	}
 
